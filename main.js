934a8e5aa779596ec8b2a78bfb183dc8d1853581
Nolan-Little <little.nolan@gmail.com>
2018-10-09 14:54:44 -0500

    add speaker reel youtube embed. add mouseover js funcionality to play no regrets

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var sheBangs = document.getElementById("bangsAudio");
 var sheMoves = document.getElementById("movesAudio");
+var noRegrets = document.getElementById("noRegretsAudio")
+var portrait = document.getElementById("hungPortrait")
 
 function playSheBangs() {
     sheBangs.play();
@@ -8,3 +10,8 @@ function playSheBangs() {
 function playSheMoves() {
   sheMoves.play();
 }
+function playNoRegrets() {
+  noRegrets.play();
+}
+
+portrait.addEventListener("mouseover", () => playNoRegrets());
